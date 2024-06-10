24d510b659ef1d6dd7354737b9bbbfa457965ba6
Motilal <motilalkayam@gmail.com>
2024-06-10 15:03:03 +0530

    day02

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat as the newer version of javascript 
+
+// alert(3 + 3) // it will give me error in node js
+// we should maintain code readablity 
+// maintaine proper gaps between the lines     
+// ecma(tc39)  --> it the standard for javascript wirtting
+// mdn --> we will use for refernces
+
+
+let name = "red" // string datatype
+let age = 20 // int datatype
+let isLoggedIn = true // boolean datatype
+// null ==> stand alone value(it is a reperesntaion of a empty value )
+// undefined --> 
+// symbols --> it is used for uniqueness( we will use it mostly on react)
+// object
+
+
+
+console.log(typeof"red") // to know the dataype of the type
+/*
+    null is object type
+    undefined is undefined type ( it is a special type )
+*/
