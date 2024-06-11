13a2dae14c0e1a9596ce77d0749cbce652a764c9
Motilal <motilalkayam@gmail.com>
2024-06-11 10:18:23 +0530

    day 4

--- 03_conversionOperators.js
+++ 03_conversionOperators.js
@@ -1,9 +1,9 @@
 let score = "33abc"
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber = Number(score) // NaN --> not a number
-console.log( valueInNumber)
+// console.log( valueInNumber)
 
 // 33 --> 33
 // "33" --> 33
@@ -15,7 +15,7 @@ console.log( valueInNumber)
 let isLoggedIn = 1;
 
 let  booleanIsLoggedIn = Boolean(isLoggedIn) // this statement is used for conversion
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 
 // 0 --> false
@@ -27,4 +27,24 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+
+
+
+
+// ************ Operations **************
+let value = 3;
+let negvalue = -value;
+//console.log(negvalue) // to print negative value
+
+let str1 = "red"
+let str2 = " pearl"
+let str3 = str1 + str2
+// console.log(str3); // this is to concatinate two strings
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
