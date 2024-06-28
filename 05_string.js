3b2c8ff0052a65e90c64cc6c3574d9b16db8a895
Motilall <moitlalkayam@gmai.com>
2024-06-28 18:20:00 +0530

    push

--- 05_string.js
+++ 05_string.js
@@ -11,4 +11,18 @@ const gameName = new String('red');// proper way of defining a string
 // console.log(gameName.length);
 // console.log(gameName.__proto__);
 // console.log(gameName.charAt(2));// what character is present in that index
-console.log(gameName.indexOf('d'));
+// console.log(gameName.indexOf('d'));
+
+// const newString = gameName.substring()
+
+const url = "https://red%20pearl.com";
+console.log(url.replace('%20', '-'));// the %20 is the automatic code for blank space 
+// the replace is used to replace the element
+
+
+
+
+
+
+
+// this is not written completely
