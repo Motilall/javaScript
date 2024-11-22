71da1c583d1d38cdb3d3fc85beaa69649cfb6554
Motilal <122986292+Motilall@users.noreply.github.com>
2024-11-22 16:46:42 +0530

    Add files via upload

---
+++ 01_let_const_var/main.js
@@ -0,0 +1,14 @@
+// console.log("hello world");
+
+const accountId = 12345
+let accountEmail = "red@gmailcom";
+var accountPassword = "777"
+// in js it is not perfered to use var because of block scope and functional scope
+accountCity = "bbsr"
+accountCity = "plr"
+// we can reseve a space for variable without declaring it (not recomended)
+let accountState;
+
+console.table([accountEmail, accountId, accountPassword, accountState]) // this is used for multiple console.log(variables) 
+
+// { } -> this is called scope
