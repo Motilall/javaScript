fd88f831f64808507e23e5b4487642ca29718eae
Motilal <122986292+Motilall@users.noreply.github.com>
2024-11-24 19:31:01 +0530

    Add files via upload

---
+++ 02_datatypes_ecma/main.js
@@ -0,0 +1,6 @@
+console.log(typeof"hello");
+"use strict"; // treate all js  code as newer version
+
+
+// datatypes are the type of values that are used to store values
+// there are 2 types of datatypes in js primiive and non-primitive(object)
